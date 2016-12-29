70b320e4eea9b8c11a5225b9173f477040422ca9
zhougithui <1528695560@qq.com>
2016-12-29 16:50:58 +0800

    theme locale timezone error fileupload

--- src/main/java/org/bear/bookstore/web/custom/CustomController.java
+++ src/main/java/org/bear/bookstore/web/custom/CustomController.java
@@ -1,23 +1,36 @@
 package org.bear.bookstore.web.custom;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
 import javax.validation.Valid;
 
 import org.bear.bookstore.domain.Custom;
 import org.bear.bookstore.service.ICustomService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
-@RestController
+@Controller
 @RequestMapping("/api/cus")
 public class CustomController {
 	@Autowired
 	private ICustomService customService;
 	
 	@RequestMapping("/getCusById/{id}")
+	@ResponseBody
 	public Custom queryCustom(@PathVariable int id){
 		Custom cus = customService.select(id);
 		return cus;
@@ -33,9 +46,67 @@ public class CustomController {
 	 * @return
 	 */
 	@RequestMapping("/getCusByName/{cusName}/{birthday}")
+	@ResponseBody
 	public Custom queryCustomByName(@Valid Custom cus, BindingResult result, 
 			@CookieValue("JSESSIONID") String cookie){
 		return cus;
 	}
 	
+	@RequestMapping("/toAdd")
+	public String toAddUser(HttpServletRequest request){
+		Locale locale = RequestContextUtils.getLocale(request);
+		TimeZone timeZone = RequestContextUtils.getTimeZone(request);
+		return "addUser";
+	}
+	
+	/**
+	 * <html>
+		    <head>
+		        <title>Upload a file please</title>
+		    </head>
+		    <body>
+		        <h1>Please upload a file</h1>
+		        <form method="post" action="/form" enctype="multipart/form-data">
+		            <input type="text" name="name"/>
+		            <input type="file" name="file"/>
+		            <input type="submit"/>
+		        </form>
+		    </body>
+		</html>
+	 */
+	/**
+	 * 如果是使用CommonsMultipartResolver，则为MultipartFile
+	 * @param name
+	 * @param file
+	 * @return
+	 * @throws IOException
+	 */
+	@RequestMapping(path = "/form", method = RequestMethod.POST)
+	public String handleFormUpload(@RequestParam("name") String name, @RequestParam("file") MultipartFile file)
+			throws IOException {
+
+		if (!file.isEmpty()) {
+			byte[] bytes = file.getBytes();
+			// store the bytes somewhere
+			return "redirect:uploadSuccess";
+		}
+
+		return "redirect:uploadFailure";
+	}
+	
+	/**
+	 * 如果是使用MultipartHttpServletRequest ，则为Part
+	 * 为servlet3新特性
+	 * @param name
+	 * @param file
+	 * @return
+	 * @throws IOException
+	 */
+	@RequestMapping(path = "/form", method = RequestMethod.POST)
+	public String handleFormUpload(@RequestParam("name") String name, @RequestParam("file") Part file)
+			throws IOException {
+		InputStream inputStream = file.getInputStream();
+		// store bytes from uploaded file somewhere
+		return "redirect:uploadSuccess";
+	}
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="s" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+<link rel="stylesheet" href="<s:theme code=''styleSheet''/>" type="text/css"/>
+</head>
 <body>
-<h2>Hello World!</h2>
+	hello world.
 </body>
 </html>
